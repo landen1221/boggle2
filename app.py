5dd15adf8b3ff07b4281c835de217562c34e299e
Matthew Landen <mlanden1221@gmail.com>
2020-11-17 14:30:49 -0800

    Made small updates to original boggle project

---
+++ app.py
@@ -0,0 +1,83 @@
+from boggle import Boggle
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secretKey"
+
+debug = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+boggle_game = Boggle()
+word_dict = boggle_game.read_dict('words.txt')
+words_found = []
+tries = 0
+score = 0
+high_score = 0
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/play-boggle')
+def setup_board():
+    global high_score
+    if tries == 0:
+        setup_game()
+    if session['score'] > high_score:
+        session['high-score'] = session['score']
+        high_score = session['score']
+    print(session)
+    return render_template('play-boggle.html', curr_game=session['curr_game'])
+
+@app.route('/word-check', methods=['POST'])
+def check_word_validity():
+    curr_word = request.form['word']
+    global tries
+    tries += 1
+
+    result = boggle_game.check_valid_word(session['curr_game'], curr_word)
+    print(f'result pre-if statement {result}')
+    if curr_word in words_found:
+        flash("You've already found that word")
+
+    elif result == 'ok':
+        words_found.append(curr_word)
+        session['found_words'] = words_found
+        session['score'] += len(result)
+        print(f'result = {result}')
+        print(f'length = {len(result)}')
+        
+    elif result == "not-on-board":
+        flash("Your word is not in the board")
+    
+    elif result == "not-word":
+        flash("Your word is not a valid word")
+    
+    
+    else:
+        flash("nothing entered")
+    
+    print(session)
+    return redirect('/play-boggle')
+
+@app.route('/restart')
+def restart_game():
+    global tries, score, words_found
+    tries = 0
+    score = 0
+    words_found = []
+    
+    return redirect('/play-boggle')
+
+
+def setup_game():
+    curr_game = boggle_game.make_board()
+    session['curr_game'] = curr_game
+    words_found = []
+    session['found_words'] = words_found
+    session['score'] = score
+    session['high-score'] = high_score
+    
---
+++ static/app.js
@@ -0,0 +1,36 @@
+// document.getElementById('add-word').addEventListener("click", function(evt) {
+//     evt.preventDefault()
+// });
+
+
+
+
+// $('#word-form').submit(function(evt) {
+//     evt.preventDefault();
+//     let word = $('#word').val();
+
+//     console.log(word)
+
+// })
+
+// let timer;
+// let count = document.getElementById('counter');
+
+// $("#counter").text(count);
+// //update display
+
+// timer = setTimeout(update, 1000);
+// //this allows for 'clearTimeout' if needed
+
+// function update()
+// {
+//     if (count > 0)
+//     {
+//        $("#counter").text(--count);
+//        timer = setTimeout(update, 1000);
+//     }
+//     else
+//     {
+//         alert("Time's Up!!!");
+//     }
+// }
